55c3ee609cd04dab78b14c3aa8dfc7793bdcfcee
lexiouellette228 <alexislouise14@icloud.com>
2025-06-23 22:51:33 -0400

    Update lightclient.py

--- Simple_Protocol/lightclient.py
+++ Simple_Protocol/lightclient.py
@@ -1,5 +1,4 @@
-4# Alexis Ouellette
-# Program Assignment 2 - Simple Protocol
+# Simple Protocol
 # lightclient.py 
 
 import socket
@@ -7,24 +6,24 @@ import argparse
 import struct
 import logging
 
-#recv_all function takes the socket to be read and then the bytes in the socket and returns the data
+# recv_all function takes the socket to be read and then the bytes in the socket and returns the data
 def recv_all(sock, length):
-    #empty object to store the number of bytes received
+    # empty object to store the number of bytes received
     data = b''  
-    #while loop to collect all the bytes
+    # while loop to collect all the bytes
     while len(data) < length:
         #get the number of bytes needed
         packet = sock.recv(length - len(data))
         #return None so the program doesn't get stuck
         if not packet:
             return None
-        #add all the data together 
+        # add all the data together 
         data += packet
-    #return new data thats been stored
+    # return new data thats been stored
     return data
 
 def main(): 
-    #Client takes 3 arguments - s: server IP, p: PORT, -l: log file location 
+    # Client takes 3 arguments - s: server IP, p: PORT, -l: log file location 
     # --command: to switch between LIGHTON and LIGHTOFF
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", required=True, help="Server IP")
@@ -33,7 +32,7 @@ def main():
     parser.add_argument("--command", choices=["LIGHTON", "LIGHTOFF"], required=True, help="Command to send")
     args = parser.parse_args()
 
-    #logging messages
+    # Logging messages
     logging.basicConfig(
         level=logging.INFO, 
         format='%(message)s',
@@ -42,27 +41,27 @@ def main():
             logging.StreamHandler()
         ])
 
-    #coonect to the server socket
+    # Connect to the server socket
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
         s.connect((args.s, args.p))
 
-        #Sending hello packet
+        # sending hello packet
         logging.info("Sending HELLO packet")
         hello_msg = b"HELLO"
         hello_header = struct.pack(">III", 17, 0, len(hello_msg))
         s.sendall(hello_header + hello_msg)
 
-        #Receiving Hello response
+        # receiving Hello response
         header = recv_all(s, 12)
         if not header:
             logging.info("Connection closed by server.")
             return
 
-        #Unpacking the header and making into 4 byte integers and logging the results
+        # Unpacking the header and making into 4 byte integers and logging the results
         version, msg_type, msg_len = struct.unpack(">III", header)
         logging.info(f"Received Data: version: {version} type: {msg_type} length: {msg_len}")
 
-        #logging version messagees
+        # logging version messagees
         if version == 17:
             logging.info("VERSION ACCEPTED")
         else: 
@@ -72,30 +71,30 @@ def main():
         if body:
             logging.info("Received Message " + body.decode())
 
-        #Send LIGHTON or LIGHTOFF command
+        # send LIGHTON or LIGHTOFF command
         logging.info("Sending command")
         command_bytes = args.command.encode()
         command_type = 1 if args.command == "LIGHTON" else 2
         cmd_header = struct.pack(">III", 17, command_type, len(command_bytes))
         s.sendall(cmd_header + command_bytes)
 
-        #receive the success message
+        # receive the success message
         header = recv_all(s, 12)
         if not header: 
             logging.info("No response received from command")
             return
         
-        #reads the header with big endian order and logs the results 
+        # reads the header with big endian order and logs the results 
         version, msg_type, msg_len = struct.unpack(">III", header)
         logging.info(f"Received data: version: {version} type: {msg_type} length: {msg_len}")
         logging.info("VERSION ACCEPTED")
 
-        #reads the message body from the socket and logs the result 
+        # reads the message body from the socket and logs the result 
         body = recv_all(s, msg_len) 
         if body: 
             logging.info("Received Message " + body.decode())
 
-        #logging closing messages 
+        # logging closing messages 
         logging.info("Command Successful")
         logging.info("Closing Socket")
 
